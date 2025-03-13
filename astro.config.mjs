ae3cd61e01de2ab11ea83bbec72308b56d680217
jhosscy <jhosscy@protonmail.com>
2025-03-13 20:46:26 +0000

    adding support node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypeMathjax from 'rehype-mathjax';
+import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [ remarkMath ],
     rehypePlugins: [ rehypeMathjax ],
+  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  server: {
+    host: '0.0.0.0'
   },
 });
